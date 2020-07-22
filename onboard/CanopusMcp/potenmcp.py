74d2a704278fb187603aad60ac2a2b5462372a13
Farid Guliyev <56725845+xRedik@users.noreply.github.com>
2020-07-22 23:53:47 +0100

    Update potenmcp.py

--- onboard/CanopusMcp/potenmcp.py
+++ onboard/CanopusMcp/potenmcp.py
@@ -5,16 +5,10 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import time
 
-# create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
-# create the cs (chip select)
 cs = digitalio.DigitalInOut(board.D25)
-
-# create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
 
 def map_func(x, in_min, in_max, out_min, out_max):
