9820a0ca620af328d56cefacb77220c7f78e6de4
Farid <feridquliyev8@gmail.com>
2020-09-11 23:20:20 +0100

    place was changed

--- onboard/canopus-telemetry/motormove.py
+++ onboard/canopus-telemetry/motormove.py
@@ -1,6 +1,6 @@
-from ctl.motorcontrol import MotorControl
+from ctl.mc.motorcontrol import MotorControl
 
 motors = MotorControl(esc_pin_A = 17, esc_pin_B = 27,
                       esc_pin_C = 22, esc_pin_D = 21)
 
-motors.calibrate()
+motors.control()
